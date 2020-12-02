be3da272a031591e7d4f8b9489b52b90fe313781
EregarMK <mkoscianek1@gmail.com>
2020-12-02 02:10:05 +0100

    full layout, chart component

---
+++ src/components/Charts/Chart.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { BarChart, ComposedChart, ResponsiveContainer, XAxis, YAxis, Tooltip, Bar, LabelList, Line, Cell } from 'recharts';
+
+const ChartType = ({ children, layout, composed, size }) => {
+    if (!composed)
+        return <ComposedChart {...size} data={data} layout={layout} barCategoryGap={4} margin={{ right: 40 }}>{children}</ComposedChart>
+    return <BarChart {...size} data={data} layout={layout} barCategoryGap={4} margin={{ left: -20, top: 20 }}>{children}</BarChart>
+}
+
+const Chart = ({ layout, composed, height }) => {
+    return (
+        <div>
+            <ResponsiveContainer>
+                <ChartType size={{ width: 400, height: height }} data={data} layout={layout} barCategoryGap={4} composed={composed}>
+                    <XAxis type={composed ? 'category' : 'number'} hide={!composed} />
+                    <YAxis type={composed ? 'number' : 'category'} hide={!composed} />
+                    <Tooltip cursor={{ fill: 'transparent', stroke: 'transparent' }} />
+                    <Bar dataKey='value' fill='#e5e7e9' stackId='stacked' >
+                        <LabelList dataKey='diff' position={composed ? 'top' : 'right'} />
+                    </Bar>
+                    <Bar dataKey='change' fill='red' stackId='stacked'>
+                        {
+                            data.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={entry.color} />
+                            ))
+                        }
+                    </Bar>
+                    {composed && <Line type='monotone' dataKey='last' stroke="#ff7300" />}
+                </ChartType>
+            </ResponsiveContainer>
+        </div>
+    );
+};
+
+const data = [
+    {
+        diff: '+4%',
+        value: 330,
+        change: 4,
+        last: 326,
+        color: '#2ABFD7'
+    },
+    {
+        diff: '+15%',
+        value: 300,
+        change: 15,
+        last: 285,
+        color: '#BDD74B'
+    },
+    {
+        diff: '+9%',
+        value: 200,
+        change: 9,
+        last: 191,
+        color: '#FF9933'
+    },
+    {
+        diff: '-17%',
+        value: 261,
+        change: -17,
+        last: 278,
+        color: '#FC5151'
+    },
+]
+
+export default Chart;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Chart from '../components/Charts/Chart';
+import MapBox from '../components/Map/MapBox';
+import ExportBoard from '../components/Panels/ExportBoard/ExportBoard';
+import Panel from '../components/Panels/Panel/Panel';
+import Select from '../components/Select/Select';
+import './App.scss';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div className='dashboard'>
+        <h2 className='dashboard-title h2'>Enterprise Shiny Dashboard</h2>
+        <div className='dashboard-panels'>
+          <Panel value='$2 674 862' name='total profit' difference='+4.5%' type='increased'>
+            <Select options={options} icon='database' boxType='primary' />
+          </Panel>
+          <Panel value='657' name='active users' difference='+8.5%' type='increased'>
+            <Select options={options} icon='user-o' boxType='success' />
+          </Panel>
+          <Panel value='$245' name='new orders' difference='+3.9%' type='increased'>
+            <Select options={options} icon='th-large' boxType='warning' />
+          </Panel>
+          <Panel value='12' name='open complaints' difference='-5.3%' type='decreased'>
+            <Select options={options} icon='ellipsis' boxType='danger' />
+          </Panel>
+        </div>
+        <div className='dashboard-boxes'>
+          <div className='dashboard-column'>
+            <div className='dashboard-box'>
+              <div className='dashboard-box-title flex'>
+                <h3>Production</h3>
+                <ExportBoard />
+              </div>
+              <Chart composed height={160} />
+            </div>
+            <div className='dashboard-box'>
+              <div className='dashboard-box-title'>
+                <h3>Summary</h3>
+              </div>
+              <Chart layout='vertical' height={100} />
+            </div>
+          </div>
+          <div className='dashboard-column'>
+            <div className='dashboard-box'>
+              <div className='dashboard-box-title flex'>
+                <h3>sales revenue by country</h3>
+                <ExportBoard />
+              </div>
+              <MapBox />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+const options = [{ name: 'Monthly Stats', id: 'monthly' }, { name: 'Yearly Stats', id: 'yearly' }];
 
 export default App;
